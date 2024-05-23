2726a03f9252179b50a45a33f7a1ae34cfff7712
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2024-05-23 13:54:14 -0300

    components created in a module

---
+++ fundamentals-components-estilizations/project-components/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { CardButtonComponent } from './card-button/card-button.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardComponent,
+    CardButtonComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
