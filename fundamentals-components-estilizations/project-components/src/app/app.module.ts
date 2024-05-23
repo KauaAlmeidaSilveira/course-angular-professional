aa31d1169d31b37b28f735f1b02c04e42a9756ac
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2024-05-23 18:18:07 -0300

    interpolation and optional chaining

--- fundamentals-components-estilizations/project-components/src/app/app.module.ts
+++ fundamentals-components-estilizations/project-components/src/app/app.module.ts
@@ -3,18 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CardComponent } from './card/card.component';
-import { CardButtonComponent } from './card-button/card-button.component';
+import { CardsModule } from './cards/cards.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent,
-    CardButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CardsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
