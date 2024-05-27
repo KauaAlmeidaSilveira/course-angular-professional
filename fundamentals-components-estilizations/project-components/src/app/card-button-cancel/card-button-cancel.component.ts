89c0c4b66c5bdb4606c833ee5ed5218ede0e4c4f
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2024-05-27 12:45:29 -0300

    testing stylesUrl with two files scss

--- fundamentals-components-estilizations/project-components/src/app/card-button-cancel/card-button-cancel.component.ts
+++ fundamentals-components-estilizations/project-components/src/app/card-button-cancel/card-button-cancel.component.ts
@@ -3,19 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card-button-cancel',
   templateUrl: './card-button-cancel.component.html',
-  styles: [
-    `
-      .card-button-cancel {
-        font-size: 16px;
-        font-weight: bold;
-        text-align: center;
-        margin-top: 10px;
-        border-radius: 25px;
-        background-color: red;
-        cursor: pointer;
-        padding: 10px;
-      }
-    `
+  styleUrls: [
+    './card-button-cancel.component.scss',
+    './card-button-cancel-2.component.scss'
   ]
 })
 export class CardButtonCancelComponent {
