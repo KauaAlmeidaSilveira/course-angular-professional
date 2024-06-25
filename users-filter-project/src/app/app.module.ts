2dc8e42502d428bdcf598e4530674c63468779e0
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2024-06-24 23:34:13 -0300

    creating user-details component with data and your style

--- users-filter-project/src/app/app.module.ts
+++ users-filter-project/src/app/app.module.ts
@@ -13,8 +13,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     ComponentsModule,
-    BrowserAnimationsModule
   ],
   providers: [
     provideAnimationsAsync()
