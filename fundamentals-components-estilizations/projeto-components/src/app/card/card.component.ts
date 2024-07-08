751661033d8b946098bb943b691c3df897f31351
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2024-07-08 17:19:52 -0300

    using get/set and transform in @Input

--- fundamentals-components-estilizations/projeto-components/src/app/card/card.component.ts
+++ fundamentals-components-estilizations/projeto-components/src/app/card/card.component.ts
@@ -6,8 +6,20 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./card.component.scss'],
 })
 export class CardComponent {
-  @Input() planType: string = '';
   @Input({required: true}) planPrice: number = 0;
+  @Input({alias: 'planType', transform: (value: string) => value.toUpperCase()}) planType: string = '';
+  
+//   Usando getter e setter, para manipular o valor do input
+//   private _planType: string = '';
+  
+//   @Input() 
+//   set planType(value: string) {
+//     this._planType = value.toUpperCase();
+//   }
+
+//   get planType(): string {
+//     return this._planType;
+//   }
 
   buttonClicked(event: boolean) {
     console.log(event ? 'V': 'F');
