0977e520530c2b140d3479961e5e931c37180702
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2024-07-09 02:17:18 -0300

    creating the component module and importing into the app component

--- fundamentals-binding-decorators-templates/cards-project/src/app/app.module.ts
+++ fundamentals-binding-decorators-templates/cards-project/src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ComponentsModule } from './components/components.module';
 import { AppComponent } from './app.component';
-import { ButtonComponent } from './components/button/button.component';
-import { CardComponent } from './components/card/card.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonComponent,
-    CardComponent
   ],
   imports: [
     BrowserModule,
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
